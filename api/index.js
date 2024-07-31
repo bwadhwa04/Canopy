5d0073a3d5b86fb45db0806998c82f6fb4296128
Bhavook <bhavukwadhwa529@gmail.com>
2024-08-01 00:42:43 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express, { response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB!");
+}).catch((err)=>{
+console.log(err);
+});
+
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('server running at 3000!');
-})
+});
