5653463808679764fc4d81829a655026e584e261
Bhavook <bhavukwadhwa529@gmail.com>
2024-08-01 01:53:44 +0530

    created a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express, { response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -15,3 +16,5 @@ const app = express();
 app.listen(3000, () => {
     console.log('server running at 3000!');
 });
+
+app.use('/api/user',userRouter);
