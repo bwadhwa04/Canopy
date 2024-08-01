57d915e1b30197e3e334de03be4bccc435f3e085
Bhavook <bhavukwadhwa529@gmail.com>
2024-08-02 01:19:02 +0530

    created signin api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
-import express, { response } from "express";
+import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB!");
   })
   .catch((err) => {
