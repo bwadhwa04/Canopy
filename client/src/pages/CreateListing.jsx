be5dd547632443be0b05295139035607f342ba48
Bhavook <bhavukwadhwa529@gmail.com>
2024-08-06 01:09:26 +0530

    added contact landlord functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -7,10 +7,10 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import { useSelector } from "react-redux";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateListing() {
-    const {currentUser}= useSelector(state =>state.user)
+  const { currentUser } = useSelector((state) => state.user);
   const [files, setFiles] = useState([]);
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
@@ -30,7 +30,7 @@ export default function CreateListing() {
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
   const [error, setError] = useState(false);
-  const [loading, setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
   console.log(formData);
   const handleImageSubmit = (e) => {
@@ -94,57 +94,66 @@ export default function CreateListing() {
   };
 
   const handleChange = (e) => {
-    if(e.target.id === 'sale' || e.target.id === 'rent'){
-        setFormData({
-            ...formData,
-            type: e.target.id,
-        })
+    if (e.target.id === "sale" || e.target.id === "rent") {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
     }
 
-    if(e.target.id === 'parking' || e.target.id === 'furnished' || e.target.id === 'offer'){
-        setFormData({
-            ...formData,
-            [e.target.id]: e.target.checked,
-        })
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
     }
 
-    if(e.target.type === 'number' || e.target.type === 'text' || e.target.type === 'textarea'){
-        setFormData({
-            ...formData,
-            [e.target.id]: e.target.value,
-        })
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
     }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        if(+formData.discountPrice > +formData.regularPrice) return setError('Discount price must be lower than regular price!')
-        if(formData.imageUrls.length<1) return setError('You must upload atleast one image');
-         setLoading(true);
-        setError(false);
-        const res = await fetch('/api/listing/create',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                ...formData,
-                userRef: currentUser._id,
-            }),
-        }
-        );
-        const data = await res.json();
-        setLoading(false);
-        if(data.success === false){
-            setError(data.message);
-        }
-        navigate(`/listing/${data._id}`);
+      if (+formData.discountPrice > +formData.regularPrice)
+        return setError("Discount price must be lower than regular price!");
+      if (formData.imageUrls.length < 1)
+        return setError("You must upload atleast one image");
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/listing/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
     } catch (error) {
-        setError(error.message);
-        setLoading(false);
+      setError(error.message);
+      setLoading(false);
     }
-  }
+  };
 
   return (
     <main className="p-3 max-w-4xl mx-auto">
@@ -272,25 +281,30 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">($ / month)</span>
+                {formData.type === "rent" && (
+                  <span className="text-xs">($ / month)</span>
+                )}
               </div>
             </div>
 
-            {formData.offer &&
-            <div className="flex items-center gap-2">
-              <input
-                type="number"
-                id="discountPrice"
-                required
-                className="p-3 border border-gray-300 rounded-lg"
-                onChange={handleChange}
-                value={formData.discountPrice}
-              />
-              <div className="flex flex-col items-center">
-                <p>Discounted price</p>
-                <span className="text-xs">($ / month)</span>
+            {formData.offer && (
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  id="discountPrice"
+                  required
+                  className="p-3 border border-gray-300 rounded-lg"
+                  onChange={handleChange}
+                  value={formData.discountPrice}
+                />
+                <div className="flex flex-col items-center">
+                  <p>Discounted price</p>
+                  {formData.type === "rent" && (
+                    <span className="text-xs">($ / month)</span>
+                  )}
+                </div>
               </div>
-            </div>}
+            )}
           </div>
         </div>
         <div className="flex flex-col flex-1 gap-4">
@@ -341,8 +355,11 @@ export default function CreateListing() {
                 </button>
               </div>
             ))}
-          <button disabled={loading || uploading} className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-            {loading? 'Creating...': 'Create listing'}
+          <button
+            disabled={loading || uploading}
+            className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            {loading ? "Creating..." : "Create listing"}
           </button>
           {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,16 +1,32 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore from 'swiper';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css/bundle';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
+import { useSelector } from "react-redux";
+import Contact from "../components/Contact.jsx";
+
+// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
   const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -32,13 +48,12 @@ export default function Listing() {
     };
     fetchListing();
   }, [params.listingId]);
-  console.log(loading);
 
   return (
     <main>
-      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
       {error && (
-        <p className='text-center my-7 text-2xl'>Something went wrong!</p>
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
       )}
       {listing && !loading && !error && (
         <div>
@@ -46,15 +61,90 @@ export default function Listing() {
             {listing.imageUrls.map((url) => (
               <SwiperSlide key={url}>
                 <div
-                  className='h-[550px]'
+                  className="h-[550px]"
                   style={{
                     background: `url(${url}) center no-repeat`,
-                    backgroundSize: 'cover',
+                    backgroundSize: "cover",
                   }}
                 ></div>
               </SwiperSlide>
             ))}
           </Swiper>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              Link copied!
+            </p>
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+            <p className="text-2xl font-semibold">
+              {listing.name} - ${" "}
+              {listing.offer
+                ? listing.discountPrice.toLocaleString("en-US")
+                : listing.regularPrice.toLocaleString("en-US")}
+              {listing.type === "rent" && " / month"}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
+              <FaMapMarkerAlt className="text-green-700" />
+              {listing.address}
+            </p>
+            <div className="flex gap-4">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {listing.type === "rent" ? "For Rent" : "For Sale"}
+              </p>
+              {listing.offer && (
+                <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                  ${+listing.regularPrice - +listing.discountPrice}
+                </p>
+              )}
+            </div>
+            <p className="text-slate-800">
+              <span className="font-semibold text-black">Description - </span>
+              {listing.description}
+            </p>
+            <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaBed className="text-lg" />
+                {listing.bedrooms > 1
+                  ? `${listing.bedrooms} beds `
+                  : `${listing.bedrooms} bed `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaBath className="text-lg" />
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} baths `
+                  : `${listing.bathrooms} bath `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaParking className="text-lg" />
+                {listing.parking ? "Parking spot" : "No Parking"}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaChair className="text-lg" />
+                {listing.furnished ? "Furnished" : "Unfurnished"}
+              </li>
+            </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
+          </div>
         </div>
       )}
     </main>
@@ -64,6 +154,7 @@ export default function Listing() {
 // import { useEffect, useState } from "react";
 // import { useParams } from "react-router-dom";
 // import { Swiper, SwiperSlide } from "swiper/react";
+// import { FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa";
 // import SwiperCore from "swiper";
 // import { Navigation } from "swiper/modules";
 // import "swiper/css/bundle";
@@ -71,7 +162,7 @@ export default function Listing() {
 // export default function Listing() {
 //   SwiperCore.use([Navigation]);
 //   const [listing, setListing] = useState(null);
-//   const [loading, setLoading] = useState(true);
+//   const [loading, setLoading] = useState(false);
 //   const [error, setError] = useState(false);
 //   const params = useParams();
 //   useEffect(() => {
@@ -90,13 +181,16 @@ export default function Listing() {
 //         setError(false);
 //       } catch (error) {
 //         setError(true);
+//         setLoading(false);
 //       }
 //     };
 //     fetchListing();
 //   }, [params.listingId]);
+//   console.log(loading);
+
 //   return (
 //     <main>
-//       {loading && <p className="text-center my-14 text-2xl">Loading...</p>}
+//       {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
 //       {error && (
 //         <p className="text-center my-7 text-2xl">Something went wrong!</p>
 //       )}
@@ -106,14 +200,119 @@ export default function Listing() {
 //             {listing.imageUrls.map((url) => (
 //               <SwiperSlide key={url}>
 //                 <div
-//                   className="h-[550px] "
-//                   style={{ background: `url(${url}) center no-repeat`, backgroundSize:'cover'}}
+//                   className="h-[500px]"
+//                   style={{
+//                     background: `url(${url}) center no-repeat`,
+//                     backgroundSize: "cover",
+//                   }}
 //                 ></div>
 //               </SwiperSlide>
 //             ))}
 //           </Swiper>
+//           <div className="p-3 md:mx-52 flex flex-col gap-4 my-6">
+//             <p className="text-2xl font-bold ">
+//               {listing.name.toUpperCase()} - $
+//               {listing.offer
+//                 ? `${listing.discountPrice}/-`
+//                 : listing.regularPrice}
+//               {listing.type === "rent" && " / month"}
+//             </p>
+//             <p className="flex  items-center my-2 gap-2 text-slate-600 text-md">
+//               <FaMapMarkerAlt className="text-green-700" />
+//               {listing.address}
+//             </p>
+//             <div className="flex gap-4">
+//               <p className="bg-red-800 w-full max-w-[200px] text-white text-center p-2 rounded-md">
+//                 {listing.type === "rent" ? "For Rent" : "For Sale"}
+//               </p>
+//               {listing.offer && (
+//                 <p className="bg-green-700 w-full max-w-[200px] text-white text-center p-2 rounded-md">
+//                   ${+listing.regularPrice - +listing.discountPrice}
+//                 </p>
+//               )}
+//             </div>
+//             <p className="text-slate-800">
+//               <span className="font-semibold text-black">Description - </span>
+//               {listing.description}
+//             </p>
+//             <ul className="text-green-900 font-semibold text-sm flex gap-4 sm:gap-6 items-center flex-wrap">
+//               <li className="flex items-center gap-1 whitespace-nowrap">
+//                 <FaBed className="text-lg" />
+//                 {listing.bedrooms > 1 ? `${listing.bedrooms} beds`: `${listing.bedrooms} bed`}
+//               </li>
+//               <li className="flex items-center gap-1 whitespace-nowrap">
+//                 <FaBath className="text-lg" />
+//                 {listing.bathrooms > 1 ? `${listing.bathrooms} baths`: `${listing.bathrooms} bath`}
+//               </li>
+//               <li className="flex items-center gap-1 whitespace-nowrap">
+//                 <FaParking className="text-lg" />
+//                 {listing.parking ? `Parking`: `No Parking`}
+//               </li>
+//               <li className="flex items-center gap-1 whitespace-nowrap">
+//                 <FaChair className="text-lg" />
+//                 {listing.furnished ? 'Furnished': 'Not Furnished'}
+//               </li>
+//             </ul>
+//           </div>
 //         </div>
 //       )}
 //     </main>
 //   );
 // }
+
+// // import { useEffect, useState } from "react";
+// // import { useParams } from "react-router-dom";
+// // import { Swiper, SwiperSlide } from "swiper/react";
+// // import SwiperCore from "swiper";
+// // import { Navigation } from "swiper/modules";
+// // import "swiper/css/bundle";
+
+// // export default function Listing() {
+// //   SwiperCore.use([Navigation]);
+// //   const [listing, setListing] = useState(null);
+// //   const [loading, setLoading] = useState(true);
+// //   const [error, setError] = useState(false);
+// //   const params = useParams();
+// //   useEffect(() => {
+// //     const fetchListing = async () => {
+// //       try {
+// //         setLoading(true);
+// //         const res = await fetch(`/api/listing/get/${params.listingId}`);
+// //         const data = await res.json();
+// //         if (data.success === false) {
+// //           setError(true);
+// //           setLoading(false);
+// //           return;
+// //         }
+// //         setListing(data);
+// //         setLoading(false);
+// //         setError(false);
+// //       } catch (error) {
+// //         setError(true);
+// //       }
+// //     };
+// //     fetchListing();
+// //   }, [params.listingId]);
+// //   return (
+// //     <main>
+// //       {loading && <p className="text-center my-14 text-2xl">Loading...</p>}
+// //       {error && (
+// //         <p className="text-center my-7 text-2xl">Something went wrong!</p>
+// //       )}
+// //       {listing && !loading && !error && (
+// //         <div>
+// //           <Swiper navigation>
+// //             {listing.imageUrls.map((url) => (
+// //               <SwiperSlide key={url}>
+// //                 <div
+// //                   className="h-[550px] "
+// //                   style={{ background: `url(${url}) center no-repeat`, backgroundSize:'cover'}}
+// //                 ></div>
+// //               </SwiperSlide>
+// //             ))}
+// //           </Swiper>
+// //         </div>
+// //       )}
+// //     </main>
+// //   );
+// // }
